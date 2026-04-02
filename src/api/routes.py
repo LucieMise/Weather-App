a0691d941c6aab68d21870bc39bfff8bf7a82ee5
Muhonja Mise <muhonjamise@gmail.com>
2026-04-02 22:31:28 +0300

    Adds Fast api routes for the weather_clients

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,34 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from requests import session
 from sqlalchemy.orm import Session 
-
-
-
-from src.api_client.multiple_cities_weather_client import multiple_cities_weather
-from src.api_client.weather_client import get_weather
-from src.database.db import SessionLocal
-from src.database.models import Weather
+from src.ingestion.multiple_city_weather_client import multiple_city_weather
+from src.ingestion.weather_client import get_weather
+from src.stoarge.database.db import SessionLocal
+from src.stoarge.database.models import Weather
 
 router = APIRouter()
-
 
 @router.get("/")
 def home():
     return {"message": "Welcome to the Weather API"}
 
-@router.get("/weather/{city}")
+@router.get("/current/{city}")
 def weather(city: str):
+    print(f"Fetching weather for {city}")
     session: Session = SessionLocal()
     weather_data = get_weather(city)
     session.close() 
     return weather_data
 
-@router.get("/weather/multiple")
-def multiple_city_weather(cities: list):
+@router.get("/multiple")
+def weather_multiple(
+    cities: str = Query(..., description="Comma-separated list of cities")
+):
+    print("Received cities:", cities)
+
     session: Session = SessionLocal()
-    weather_data = multiple_cities_weather(cities)
-    session.close()
-    return weather_data
+    try:
+        weather_data = multiple_city_weather(cities)
+        return weather_data
+    finally:
+        session.close()
 
 
 
